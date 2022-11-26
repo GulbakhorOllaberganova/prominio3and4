692efedb6e04ad3b9b58d1807bc529c47b68fbdb
Guli <90575705+GulbakhorOllaberganova@users.noreply.github.com>
2022-11-26 12:45:41 -0800

    Update Week3and4.java
    I fixed exercise 12.  corrected "=="

--- Week3and4.java
+++ Week3and4.java
@@ -134,7 +134,7 @@ nameLengths[i]=names[i].length();
 	}
 		//12.	Write a method called willBuyDrink that takes a boolean isHotOutside, and a double moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
 		public static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) {
-			if(isHotOutside= true && moneyInPocket>10.50) {
+			if(isHotOutside== true && moneyInPocket>10.50) {
 				return true;
 			}else {
 				return false;
